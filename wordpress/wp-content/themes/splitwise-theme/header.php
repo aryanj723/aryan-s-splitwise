9e2fc4d0109aa5b6e0efad131df0ccc8ce224dde
Aryan <aryanj723@protonmail.com>
2024-08-18 18:03:56 +0530

    Add some theme sortings

--- wordpress/wp-content/themes/splitwise-theme/header.php
+++ wordpress/wp-content/themes/splitwise-theme/header.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name'); ?></title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     <?php wp_head(); ?>
